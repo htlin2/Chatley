7c47d69db1f197c8225fc8a0a0c2583f03a166a0
htlin2 <htlin2@gmail.com>
2018-12-01 22:11:53 -0800

    added eslint

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+	"parser": "babel-eslint",
+	"extends": "airbnb",
+	"env": {
+		"browser": true,
+		"node": true,
+		"es6": true,
+	},
+	"rules": {
+		"no-console": "off"
+	},
+}
